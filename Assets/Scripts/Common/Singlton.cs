354dcacb6ed96d33dbac829b0be0f3e78d8b5edb
wlmgit <1621020745@qq.com>
2017-09-19 11:52:32 +0800

    Singlton Signed-off-by: wlmgit <1621020745@qq.com>

---
+++ Assets/Scripts/Common/Singlton.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Framework_Common
+{
+    public abstract class Singleton<T> where T : new()
+    {
+        private static T m_instance;
+        private static readonly object m_lock = new object();//防止多线程修改
+
+        public static T Instance
+        {
+            get
+            {
+                if (m_instance == null)
+                {
+                    lock (m_lock)
+                    {
+                        if( m_instance ==null )
+                            m_instance = new T();
+                    }
+                }
+                return m_instance;
+            }
+        }
+    }
+}
+
