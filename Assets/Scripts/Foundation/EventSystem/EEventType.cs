f84ef75ed53e4d603969238fc8804186bbb24296
wlmgit <1621020745@qq.com>
2017-11-06 18:28:35 +0800

    Fist add

---
+++ Assets/Scripts/Foundation/EventSystem/EEventType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Foundation.Event
+{
+    /// <summary>
+    /// 不同的项目使用需要自定义不同的事件类型
+    /// </summary>
+    public enum EEventType
+    {
+        Click,
+        Drag,
+        DoubleClick
+    }
+}
